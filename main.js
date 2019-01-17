9534cef01a2009a1f980fdb67287d53cf012c92d
Anna Ridenour <annamridenour@gmail.com>
2019-01-16 20:43:27 -0800

    all the updates

--- main.js
+++ main.js
@@ -1,2 +1,38 @@
-var sum = 3 + 4;
-console.log('hello console!', sum);
+var x = 15;
+
+if(x > 12) {
+    console.log(x);
+}
+else {
+    console.log('too small');
+
+}
+
+for(var i = 0; i < x; i++) {
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}
+
+function sayHello() {
+    console.log('hi');
+}
+
+if(x % 2 === 0) {
+    sayHello();
+}
+
+// eslint-disable-next-line
+function evenOrOdd() {
+    var input = document.getElementById('number');
+    console.log(input.value);
+
+    var y = input.value;
+
+    if(y % 2 === 0) {
+        console.log('even');
+    }
+    else {
+        console.log('odd')
+    }
+}
